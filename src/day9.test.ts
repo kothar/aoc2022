309b3a6a669027990b4fc1b6e367f9643226715e
Mike Houston <michael.houston4@nhs.net>
2022-12-09 09:39:05 +0000

    Day 9 part 1

--- src/day9.test.ts
+++ src/day9.test.ts
@@ -1,10 +1,55 @@
 import * as fs from 'fs';
-import { day9 } from './day9';
+import { day9, State } from './day9';
+
+describe('applyMove', () => {
+    test('moves up', () => {
+        expect(new State({ x: 0, y: 1 }, { x: 0, y: 0 }).applyMove({ x: 0, y: 1 }, 1))
+            .toEqual([{ x: 0, y: 2 }, { x: 0, y: 1 }]);
+    })
+    test('moves down', () => {
+        expect(new State({ x: 0, y: -1 }, { x: 0, y: 0 }).applyMove({ x: 0, y: -1 }, 1))
+            .toEqual([{ x: 0, y: -2 }, { x: 0, y: -1 }]);
+    })
+    test('moves right', () => {
+        expect(new State({ x: 1, y: 0 }, { x: 0, y: 0 }).applyMove({ x: 1, y: 0 }, 1))
+            .toEqual([{ x: 2, y: 0 }, { x: 1, y: 0 }]);
+    })
+    test('moves left', () => {
+        expect(new State({ x: -1, y: 0 }, { x: 0, y: 0 }).applyMove({ x: -1, y: 0 }, 1))
+            .toEqual([{ x: -2, y: 0 }, { x: -1, y: 0 }]);
+    })
+    test('moves up+right', () => {
+        expect(new State({ x: 1, y: 1 }, { x: 0, y: 0 }).applyMove({ x: 0, y: 1 }, 1))
+            .toEqual([{ x: 1, y: 2 }, { x: 1, y: 1 }]);
+    })
+})
 
 test('Example 1', () => {
     const result = day9(`
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
 `);
-    expect(result).toEqual([21, 8]);
+    expect(result).toEqual([13, 1]);
+});
+
+test('Example 2', () => {
+    const result = day9(`
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+`);
+    expect(result).toEqual([13, 36]);
 });
 
 test('Puzzle input', () => {
--- src/day9.ts
+++ src/day9.ts
@@ -1,4 +1,70 @@
-export function day9(input: string) {
+export type Position = { x: number, y: number };
 
-    return [0,0];
+export class State {
+
+    visited = new Set<string>();
+
+    constructor(public head: Position = { x: 0, y: 0 }, public tail: Position = { x: 0, y: 0 }) {
+        this.visited.add(JSON.stringify(tail));
+    }
+
+    distance(a, b) {
+        return b - a;
+    }
+
+    maxDistance() {
+        return Math.max(
+            Math.abs(this.distance(this.head.x, this.tail.x)),
+            Math.abs(this.distance(this.head.y, this.tail.y))
+        );
+    }
+
+    function
+
+    applyMove({ x, y }: Position, distance: number) {
+        for (let step = 0; step < distance; step++) {
+            this.head = { x: this.head.x + x, y: this.head.y + y };
+            if (this.maxDistance() > 1) {
+                if (this.head.x > this.tail.x) {
+                    this.tail.x += 1
+                } else if (this.head.x < this.tail.x) {
+                    this.tail.x -= 1
+                }
+                if (this.head.y > this.tail.y) {
+                    this.tail.y += 1
+                } else if (this.head.y < this.tail.y) {
+                    this.tail.y -= 1
+                }
+                this.visited.add(JSON.stringify(this.tail));
+            }
+        }
+        return [this.head, this.tail];
+    }
+}
+
+export function day9(input: string) {
+    const moves = input.trim().split('\n')
+        .map(line => line.split(' '))
+        .map(([direction, distance]) => ({ direction, distance: parseInt(distance) }));
+
+    const state = new State();
+
+    for (const move of moves) {
+        switch (move.direction) {
+            case 'U':
+                state.applyMove({ x: 0, y: 1 }, move.distance);
+                break;
+            case 'D':
+                state.applyMove({ x: 0, y: -1 }, move.distance);
+                break;
+            case 'L':
+                state.applyMove({ x: -1, y: 0 }, move.distance);
+                break;
+            case 'R':
+                state.applyMove({ x: 1, y: 0 }, move.distance);
+                break;
+        }
+    }
+
+    return [state.visited.size, 0];
 }
